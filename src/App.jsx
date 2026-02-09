420c87f05c4a5e0e5dd346928aa1ec331aef2934
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-02-09 12:31:16 +0545

    feat(routing): add page naviagtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import JobListingPage from "./pages/JobListingPage";
+import JobDetailsPage from "./pages/JobDetailsPage";
 
 const App = () => {
-  return <div className="text-red-900">App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<JobListingPage />} />
+        <Route path="/details/:id" element={<JobDetailsPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
